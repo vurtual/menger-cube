635b02d5cdaecd2cd053e1d2e096611519989d01
Andrew Sharpe <andrew@sharpe-thinking.tech>
2022-07-11 08:23:39 +0100

    added mouse based rotation

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ let angle={x:-35,y:10,z:0}
 
 function setup() {
     createCanvas(window.innerWidth-50 ,window.innerHeight-50  , WEBGL)
-    Cube(1000, createVector(460,-300,-800))
+    Cube(400, createVector(0,0,0))
     noStroke()
     }
 
 function draw() {
     clear(0, 0, 0, 1)
-   
+    rotateX(-mouseY/400 - height / 2)
+    rotateZ(mouseX/400 - width/2)
     ambientLight(60, 60, 60);
     pointLight(255, 255, 255, 500,500, 100);
   
-    rotateX(angle.x)
-    rotateY(angle.y)
-    rotateZ(angle.z)   
+    // rotateX(angle.x)
+    // rotateY(angle.y)
+    // rotateZ(angle.z)   
 
     cubes.forEach(cube => cube.draw())
     // angle.x += 0.003
